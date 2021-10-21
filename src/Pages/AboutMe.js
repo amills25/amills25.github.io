d72bc89009262c6ea2011b03b2d9a4fdf78800ec
awmills25 <awmills25@gmail.com>
2021-10-21 16:34:14 -0400

    added background

--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -1,26 +1,51 @@
-import React from 'react';
+import React from "react";
 
 export default function AboutMe() {
     return (
         <>
-            <img src="./img/AMBlackSmallTrans.jpg" class="rounded img-fluid mx-auto d-block" alt="Small Black Logo" />
+            <img
+                src="./img/AMBlackSmallTrans.jpg"
+                className="rounded img-fluid mx-auto d-block"
+                alt="Small Black Logo"
+            />
 
-            <h1 class="text-center">ABOUT ME</h1>
+            <h1 className="text-center">ABOUT ME</h1>
 
-            <div class="container border border-dark border-5 rounded-3 bg-white">
-                <nav class="navbar navbar-expand-lg navbar-light">
-                    <div class="container-fluid">
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
-                            aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
+            <div className="container border border-dark border-5 rounded-3 bg-white">
+                <nav className="navbar navbar-expand-lg navbar-light">
+                    <div className="container-fluid">
+                        <button
+                            className="navbar-toggler"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#navbarNavAltMarkup"
+                            aria-controls="navbarNavAltMarkup"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
+                            <span className="navbar-toggler-icon"></span>
                         </button>
-                        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                            <div class="navbar-nav">
-                                <a class="nav-link fw-bold" href="#">Home</a>
-                                <a class="nav-link fw-bold" aria-current="page" href="#">About</a>
-                                <a class="nav-link fw-bold" href="#">Blog</a>
-                                <a class="nav-link fw-bold" href="#">Portfolio</a>
+                        <div
+                            className="collapse navbar-collapse"
+                            id="navbarNavAltMarkup"
+                        >
+                            <div className="navbar-nav">
+                                <a className="nav-link fw-bold" href="#">
+                                    Home
+                                </a>
+                                <a
+                                    className="nav-link fw-bold"
+                                    aria-current="page"
+                                    href="#"
+                                >
+                                    About
+                                </a>
+                                <a className="nav-link fw-bold" href="#">
+                                    Blog
+                                </a>
+                                <a className="nav-link fw-bold" href="#">
+                                    Portfolio
+                                </a>
                             </div>
                         </div>
                     </div>
@@ -34,9 +59,13 @@ export default function AboutMe() {
                     <div className="col"></div>
                     <h2 className="text-center">Andrew Mills</h2>
                     <div className="text-center">
-                        <img src="./img/glory.png" className="rounded img-fluid" alt="Image of Andrew" />
+                        <img
+                            src="./img/glory.png"
+                            className="rounded img-fluid"
+                            alt="Image of Andrew"
+                        />
                     </div>
-                    
+
                     <h3>Currently, I'm:</h3>
                     <ul>An aspiring web developer</ul>
 
@@ -49,13 +78,17 @@ export default function AboutMe() {
                     <ul>Playing fantasy sports</ul>
                     <ul>Cheering on the Kentucky Wildcats</ul>
                     <ul>Trying out new restaurants</ul>
-
                 </div>
             </div>
 
             <div className="row-1">
                 <div className="col text-center">
-                    <img src="./img/AMBlackSmallTrans.jpg" alt="Andrew Mills Logo" width="30" height="24" />
+                    <img
+                        src="./img/AMBlackSmallTrans.jpg"
+                        alt="Andrew Mills Logo"
+                        width="30"
+                        height="24"
+                    />
                     Developed by Andrew Mills, 2021.
                 </div>
             </div>
