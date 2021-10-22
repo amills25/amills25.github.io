53b6feb9b24f2b470f9a00acacb1109c69e85c52
awmills25 <awmills25@gmail.com>
2021-10-22 15:27:07 -0400

    turn blog into react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import AboutMe from './Pages/AboutMe';
+import React from "react";
+import AboutMe from "./Pages/AboutMe";
+import Blog from "./Pages/Blog";
 
 export default function App() {
-  return (
-    <div className="App">
-        <AboutMe />
-    </div>
-  );
+    return (
+        <div className="App">
+            <AboutMe />
+            <Blog />
+        </div>
+    );
 }
