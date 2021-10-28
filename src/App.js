4a5b3229deb654af9ed231315a0613d825a37979
awmills25 <awmills25@gmail.com>
2021-10-28 15:34:30 -0400

    added react router to app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React from "react";
+import Home from "./Pages/Home";
 import AboutMe from "./Pages/AboutMe";
 import Blog from "./Pages/Blog";
 import Projects from "./Pages/Projects";
 import Contact from "./Pages/Contact";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function App() {
     return (
-        <div className="App">
-            <>
-                <AboutMe />
-                <Projects />
-                <Blog />
-                {/* <Contact /> */}
-            </>
-        </div>
+        <Router>
+            <div>
+                {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                <Switch>
+                    <Route path="/about">
+                        <AboutMe />
+                    </Route>
+                    <Route path="/projects">
+                        <Projects />
+                    </Route>
+                    <Route path="/blog">
+                        <Blog />
+                    </Route>
+                    <Route path="/contact">
+                        <Contact />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
